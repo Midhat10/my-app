94671e5c6e62da3d6ba21f3a341d87c41b958ce7
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-02-11 03:49:23 +0300

    Работа с пересечениями, устарел тайпскрипт до 4.9 версии

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useRef, useState } from "react";
 import Hover from "./components/Hover.tsx";
 import useHover from "./hooks/userHover.js";
+import List from "./components/List.tsx";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <List />
+    </div>
+  );
 }
 
 export default App;
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,17 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import ky from "ky";
 
 const List = () => {
   const [todos, setTodos] = useState([]);
+  const [page, setPage] = useState(1);
+  const limit = 20;
+  const parentRef = useRef();
+  const childRef = useRef();
 
-  const ky = () => {};
+  const kyTodos = async (page, limit) => {
+    try {
+      const todos = await ky(
+        `https://jsonplaceholder.typicode.com/todos/?_limit=${limit}&_page=${page}`,
+      ).json();
+      setTodos(todos);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  useEffect(() => {
+    kyTodos(page, limit);
+  }, []);
 
   return (
-    <div>
+    <div ref={parentRef}>
       {todos.map((todo) => (
-        <div style={{ padding: 30, border: "2px solid black" }}>
-          {todo.title}
+        <div key={todo.id} style={{ padding: 30, border: "2px solid black" }}>
+          {todo.id} {todo.title}
         </div>
       ))}
+      <div ref={childRef} style={{ height: 20, backgroundColor: "green" }} />
     </div>
   );
 };
