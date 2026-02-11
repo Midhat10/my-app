7d4fa0dd31cbf289d906bbd054cba2e3207b36cb
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-02-11 03:58:55 +0300

    Сделал пагинацию. Я красавчик

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import ky from "ky";
+import useScroll from "../hooks/useScroll";
 
 const List = () => {
   const [todos, setTodos] = useState([]);
@@ -7,24 +8,24 @@ const List = () => {
   const limit = 20;
   const parentRef = useRef();
   const childRef = useRef();
+  const intersected = useScroll(parentRef, childRef, () =>
+    kyTodos(page, limit),
+  );
 
-  const kyTodos = async (page, limit) => {
+  async function kyTodos(page, limit) {
     try {
       const todos = await ky(
         `https://jsonplaceholder.typicode.com/todos/?_limit=${limit}&_page=${page}`,
       ).json();
-      setTodos(todos);
+      setTodos((prev) => [...prev, ...todos]);
+      setPage((prev) => prev + 1);
     } catch (e) {
       console.log(e.message);
     }
-  };
-
-  useEffect(() => {
-    kyTodos(page, limit);
-  }, []);
+  }
 
   return (
-    <div ref={parentRef}>
+    <div ref={parentRef} style={{ height: "80vh", overflow: "auto" }}>
       {todos.map((todo) => (
         <div key={todo.id} style={{ padding: 30, border: "2px solid black" }}>
           {todo.id} {todo.title}
