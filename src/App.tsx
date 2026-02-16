32359411e877a4932e4097a0f7705efdd9e73792
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2026-02-16 18:04:13 +0300

    После пагинации, сделал хук дебоунсед

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,30 @@ import { useRef, useState } from "react";
 import Hover from "./components/Hover.tsx";
 import useHover from "./hooks/userHover.js";
 import List from "./components/List.tsx";
+import ky from "ky";
+import useDebounce from "./hooks/useDebounce.js";
 
 function App() {
+  const [value, setValue] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+  async function search(query) {
+    try {
+      const todos = await ky(
+        `https://jsonplaceholder.typicode.com/todos?query=` + query,
+      ).json();
+      console.log(todos);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+    debouncedSearch(e.target.value);
+  };
   return (
     <div>
-      <List />
+      <input type="text" value={value} onChange={onChange} />
     </div>
   );
 }
---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,20 @@
+import React, { useRef, useCallback } from "react";
+
+function useDebounce(callback, delay) {
+  const timer = useRef();
+
+  const debouncedCallback = useCallback(
+    (...args) => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+      }
+      timer.current = setTimeout(() => {
+        callback(...args);
+      }, delay);
+    },
+    [callback, delay],
+  );
+  return debouncedCallback;
+}
+
+export default useDebounce;
